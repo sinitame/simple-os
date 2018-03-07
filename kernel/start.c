f2b192f821f1cf28aa1d0b21e9946133a7e7214a
Bouilhac Benjamin <benjamin.bouilhac@grenoble-inp.org>
2018-03-07 12:41:56 +0100

    test0 correctement importe de user vers kernel
    il faut basculer manuellement chaque test du mode user au mode kernel
    donc copier le code dans un nouveau .c, creer un .h et l'inclure dans
    kernel_start

--- kernel/start.c
+++ kernel/start.c
@@ -1,41 +1,13 @@
-#include "debugger.h"
 #include "cpu.h"
-#include <stdio.h>
-#include "../shared/console.h"
-#include <inttypes.h>
-#include <start.h>
-#include "../user/lib/clock.h"
-
-int fact(int n)
-{
-	if (n < 2)
-		return 1;
-
-	return n * fact(n-1);
-}
-
+#include "test0.h"
+#include "stdio.h"
+#include "console.h"
 
 void kernel_start(void)
 {
-	int i;
-//	call_debugger();
-
-	i = 10;
-
-	i = fact(i);
-
 	efface_ecran();
+	test0(0);
 
-	printf("Hello world !");
-	printf("i= %d", i);
-
-	clk();
-	masque_IRQ(0,0);
-	init_traitant_IT(32, traitant_IT_32);
-	sti();
-
-
-	affiche_h();
 	while(1)
 	  hlt();
 
---
+++ kernel/test0.h
@@ -0,0 +1,6 @@
+#ifndef __TEST0_H__
+#define __TEST0_H__
+
+extern int test0(void *arg);
+
+#endif
