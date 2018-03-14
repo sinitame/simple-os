bd70500b584838ba8cb9629e85a255babfa976e0
Bouilhac Benjamin <benjamin.bouilhac@grenoble-inp.org>
2018-03-14 11:43:00 +0100

    Ajout de test1.c dans kernel
    branch processus_ordonnancement non encore mergee avec test
    donc test1 ne fonctionne pas encore

---
+++ kernel/test1.c
@@ -0,0 +1,48 @@
+/*******************************************************************************
+ * Test 1
+ *
+ * Demarrage de processus avec passage de parametre
+ * Terminaison normale avec valeur de retour
+ * Attente de terminaison (cas fils avant pere et cas pere avant fils)
+ ******************************************************************************/
+
+#include "test1.h"
+
+int dummy2(void *arg)
+{
+        printf(" 5");
+        assert((int) arg == DUMMY_VAL + 1);
+        return 4;
+}
+
+int dummy1(void *arg) {
+        printf("1");
+        assert((int) arg == DUMMY_VAL);
+        return 3;
+}
+
+
+int test1(void *arg)
+{
+        int pid1;
+        int r;
+        int rval;
+
+        (void)arg;
+
+        pid1 = start(dummy1, "dummy1", 4000, 192);
+        assert(pid1 > 0);
+        printf(" 2");
+        r = waitpid(pid1, &rval);
+        assert(r == pid1);
+        assert(rval == 3);
+        printf(" 3");
+        pid1 = start(dummy2, "dummy2", 4000, 100);
+        assert(pid1 > 0);
+        printf(" 4");
+        r = waitpid(pid1, &rval);
+        assert(r == pid1);
+        assert(rval == 4);
+        printf(" 6.\n");
+        return 0;
+}
---
+++ kernel/test1.h
@@ -0,0 +1,7 @@
+#ifndef _TEST1_H_
+#define _TEST1_H_
+
+#define DUMMY_VAL 78
+extern int test1(void *arg);
+
+#endif /* _TEST1_H_ */
