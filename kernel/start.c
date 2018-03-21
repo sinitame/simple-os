781a181fc5787d0164416643a161d4c6d2705cbf
Bouilhac Benjamin <benjamin.bouilhac@grenoble-inp.org>
2018-03-21 12:23:05 +0100

    Modification de test1 pour conformite au test user
    voir TODO dans test1.c car les appels de processus ne sont pas
    conformes a la spec
    menage dans kernel_start qui ne lance que test0 (print)

--- kernel/start.c
+++ kernel/start.c
@@ -1,25 +1,11 @@
 #include "cpu.h"
 #include "test0.h"
-#include "test1.h"
 #include "stdio.h"
 #include "../shared/console.h"
 #include <inttypes.h>
 #include <start.h>
 #include "../user/lib/clock.h"
 #include "processus.h"
-
-// static void dummy2(void)
-// {
-//         printf(" 5");
-//         // assert((int) arg == DUMMY_VAL + 1);
-//         // return 4;
-// }
-//
-// static void dummy1(void) {
-//         printf("1");
-//         // assert((int) arg == DUMMY_VAL);
-//         // return 3;
-// }
 
 void kernel_start(void)
 {
@@ -29,14 +15,8 @@ void kernel_start(void)
 	init_traitant_IT(32, traitant_IT_32);
 
 	efface_ecran();
-	//Initialisation de idle
-	//Lancement de idle (Il va ensuite lancer les autres)
-	init_idle();
-	start(proc1, "dummy1", 4000, 192);
-	start(proc2, "dummy2", 4000, 100);
-
 	sti();
-	idle();
+	test0(0);
 	while(1)
 	  hlt();
 
--- kernel/test1.c
+++ kernel/test1.c
@@ -6,48 +6,57 @@
  * Attente de terminaison (cas fils avant pere et cas pere avant fils)
  ******************************************************************************/
 
-#include "stdio.h"
-#include "processus.h"
-#define DUMMY_VAL 78
+/*
+TODO
+0) il faudra penser a changer le prototype de start quand on aura la memoire virtuelle
+0-bis) le prototype Apprenti est incomplet : on doit pouvoir passer des parametres + le prototype du processus est int (void*)
+moral test1.c doit marcher en changeant le prototype de start
 
-void dummy2(void)
-{
-        printf(" 5");
-        // assert((int) arg == DUMMY_VAL + 1);
-        // return 4;
-}
+1) la terminaison automatique des processus ne marche pas : appeler proc1, proc2 resout le probleme
+il faut ajouter le code de terminaison dans la pile lors de la creation des processus
 
-void dummy1(void) {
-        printf("1");
-        // assert((int) arg == DUMMY_VAL);
-        // return 3;
-}
+2) il y a aussi un pb sur idle car on ne peut pas executer le code etant donne que l'appel a idle est bloquant
+*/
 
 
-int test1(void *arg)
-{
-        // int pid1;
-        // int r;
-        // int rval;
-
-        (void)arg;
-
-        init_idle();
-        start(dummy1, "dummy1", 4000, 192);
-        start(dummy2, "dummy2", 4000, 100);
-        idle();
-        // assert(pid1 > 0);
-        // printf(" 2");
-        // r = waitpid(pid1, &rval);
-        // assert(r == pid1);
-        // assert(rval == 3);
-        // printf(" 3");
-        // pid1 = start(dummy2, "dummy2", 4000, 100);
-        // assert(pid1 > 0);
-        // printf(" 4");
-        // r = waitpid(pid1, &rval);
-        // assert(r == pid1);
-        // assert(rval == 4);
-        // printf(" 6.\n");
-        return 0;
-}
+// #include "stdio.h"
+// #include "processus.h"
+// #define DUMMY_VAL 78
+//
+// int dummy2(void *arg)
+// {
+//         printf(" 5");
+//         assert((int) arg == DUMMY_VAL + 1);
+//         return 4;
+// }
+//
+// int dummy1(void *arg) {
+//         printf("1");
+//         assert((int) arg == DUMMY_VAL);
+//         return 3;
+// }
+//
+// int test1(void *arg)
+// {
+//         int pid1;
+//         int r;
+//         int rval;
+//
+//         (void)arg;
+//
+//         pid1 = start("dummy1", 4000, 192, (void *) DUMMY_VAL);
+//         assert(pid1 > 0);
+//         printf(" 2");
+//         r = waitpid(pid1, &rval);
+//         assert(r == pid1);
+//         assert(rval == 3);
+//         printf(" 3");
+//         pid1 = start("dummy2", 4000, 100, (void *) (DUMMY_VAL + 1));
+//         assert(pid1 > 0);
+//         printf(" 4");
+//         r = waitpid(pid1, &rval);
+//         assert(r == pid1);
+//         assert(rval == 4);
+//         printf(" 6.\n");
+//         return 0;
+// }
