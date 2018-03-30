9b9c2342e9b30c766379a68b82f18b882137e55a
Théo DAVID <theo.david@grenoble-inp.org>
2018-03-30 08:09:26 +0200

    [Titre: Ajout test10, test 9 pas implemente]

---
+++ kernel/test10.h
@@ -0,0 +1,21 @@
+// #ifndef _TEST10_H_
+// #define _TEST10_H_
+//
+// #if defined WITH_SEM
+//
+// int test10(void *arg);
+//
+// #elif defined WITH_MSG
+//
+// int test10(void *arg);
+//
+// static void write(int fid, const char *buf, unsigned long len);
+//
+// static void read(int fid, char *buf, unsigned long len);
+//
+// #else
+//
+// # error "WITH_SEM" ou "WITH_MSG" doit être définie.
+// #endif
+//
+// #endif _TEST10_H_
