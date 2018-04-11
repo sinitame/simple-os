f0503bef3818b0620131d6acb03fdc098740debc
Théo DAVID <theo.david@grenoble-inp.org>
2018-04-11 11:59:30 +0200

    test5 compile mais s'execute mal

--- kernel/start.c
+++ kernel/start.c
@@ -25,7 +25,8 @@ void kernel_start(void)
 	// start(test1, STACK_LENGTH, 128, "test1", (void *) 77);
 	// start(test2, STACK_LENGTH, 128, "test2", (void *) 77);
 	// start(test3, STACK_LENGTH, 128, "test3", (void *) 77);
-	start(test4, STACK_LENGTH, 128, "test4", (void *) 77);
+	// start(test4, STACK_LENGTH, 128, "test4", (void *) 77);
+	start(test5, STACK_LENGTH, 128, "test5", (void *) 77);
 
 	idle();
 
