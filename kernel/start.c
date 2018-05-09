955807a7a1d52c86fac0e8c5221f3c00efba3f3e
Benjamin Bouilhac <Benjamin.Bouilhac@grenoble-inp.fr>
2018-05-09 09:13:12 +0200

    Modification architecture des tests niveau kernel
    ajout de tests/ dans kernel/ et modification de kernel/Makefile
    -> il faut rajouter chaque subdir dans $DIR sinon le Makefile n'y va pas

--- kernel/start.c
+++ kernel/start.c
@@ -9,7 +9,7 @@
 #include "../user/lib/clock.h"
 #include "processus.h"
 
-#include "tests.h"
+#include "tests/tests.h"
 
 void kernel_start(void)
 {
@@ -22,21 +22,22 @@ void kernel_start(void)
 
 	init_idle();
 
-	// start(test1, 4000, 128, "test1", (void *) 77);
-	// start(test2, 4000, 128, "test2", (void *) 77);
-	// start(test3, 4000, 128, "test3", (void *) 77);
-	// start(test4, 4000, 128, "test4", (void *) 77);
+	start(test0, 4000, 128, "test0", (void *) 77);
+	start(test1, 4000, 128, "test1", (void *) 77);
+	start(test2, 4000, 128, "test2", (void *) 77);
+	start(test3, 4000, 128, "test3", (void *) 77);
+	start(test4, 4000, 128, "test4", (void *) 77);
 	// start(test5, 4000, 128, "test5", (void *) 77);
 	// start(test6, 4000, 128, "test6", (void *) 77);
-	// start(test7, 4000, 128, "test7", (void* ) 77); -- à vérifier
+	// start(test7, 4000, 128, "test7", (void* ) 77);
 	// start(test8, 4000, 128, "test8", (void *) 77);
-	// start(test9, 4000, 128, "test9", (void*) 77);  -- à vérifier
+	// start(test9, 4000, 128, "test9", (void*) 77);
 	// start(test12, 4000, 128, "test12", (void *) 77);
-	//start(test13, 4000, 128, "test13", (void*) 77);
-	//start(test14, 4000, 128, "test14", (void *) 77);
-	//start(test15, 4000, 128, "test15", (void *) 77);
-	//start(test16, 4000, 128, "test16", (void*)77);
-	//start(test17, 4000, 128, "test17", (void*)77);
+	// start(test13, 4000, 128, "test13", (void*) 77);
+	// start(test14, 4000, 128, "test14", (void *) 77);
+	// start(test15, 4000, 128, "test15", (void *) 77);
+	// start(test16, 4000, 128, "test16", (void*)77);
+	// start(test17, 4000, 128, "test17", (void*)77);
 
 	idle();
 
