7cff51f837d74b0d73f7da7241c18bece2864ef4
Benjamin Bouilhac <Benjamin.Bouilhac@grenoble-inp.fr>
2018-05-09 09:30:00 +0200

    nieme verification des tests
    testi pour i dans 0..6, 8, 12, 14, 15, 17 ok
    test7 et 9 ok aussi mais tres long
    - test7 changer les deux wait_clock
    - test9 changer les bornes sup des for
    
    test13 et 16 sont encore a debugger

--- kernel/start.c
+++ kernel/start.c
@@ -27,15 +27,15 @@ void kernel_start(void)
 	start(test2, 4000, 128, "test2", (void *) 77);
 	start(test3, 4000, 128, "test3", (void *) 77);
 	start(test4, 4000, 128, "test4", (void *) 77);
-	// start(test5, 4000, 128, "test5", (void *) 77);
-	// start(test6, 4000, 128, "test6", (void *) 77);
+	start(test5, 4000, 128, "test5", (void *) 77);
+	start(test6, 4000, 128, "test6", (void *) 77);
 	// start(test7, 4000, 128, "test7", (void* ) 77);
-	// start(test8, 4000, 128, "test8", (void *) 77);
+	start(test8, 4000, 128, "test8", (void *) 77);
 	// start(test9, 4000, 128, "test9", (void*) 77);
-	// start(test12, 4000, 128, "test12", (void *) 77);
+	start(test12, 4000, 128, "test12", (void *) 77);
 	// start(test13, 4000, 128, "test13", (void*) 77);
-	// start(test14, 4000, 128, "test14", (void *) 77);
-	// start(test15, 4000, 128, "test15", (void *) 77);
+	start(test14, 4000, 128, "test14", (void *) 77);
+	start(test15, 4000, 128, "test15", (void *) 77);
 	// start(test16, 4000, 128, "test16", (void*)77);
 	// start(test17, 4000, 128, "test17", (void*)77);
 
