3c17a2a9d1b10658ef141d2724e4f5410d94b3bd
Benjamin Bouilhac <Benjamin.Bouilhac@grenoble-inp.fr>
2018-05-09 22:58:56 +0200

    preparation pour rendu

--- kernel/start.c
+++ kernel/start.c
@@ -37,20 +37,21 @@ void kernel_start(void)
 
 	init_idle();
 
-	sti();
+    /* processus */
+	start(test0, 4000, 128, "test0", (void *) 77);
+	start(test1, 4000, 128, "test1", (void *) 77);
+	start(test2, 4000, 128, "test2", (void *) 77);
+	start(test3, 4000, 128, "test3", (void *) 77);
+	start(test4, 4000, 128, "test4", (void *) 77);
+	start(test5, 4000, 128, "test5", (void *) 77);
+	start(test6, 4000, 128, "test6", (void *) 77);
 
-	// start(test0, 4000, 128, "test0", (void *) 77);
-	// start(test1, 4000, 128, "test1", (void *) 77);
-	// start(test2, 4000, 128, "test2", (void *) 77);
-	// start(test3, 4000, 128, "test3", (void *) 77);
-	// start(test4, 4000, 128, "test4", (void *) 77);
-	// start(test5, 4000, 128, "test5", (void *) 77);
-	// start(test6, 4000, 128, "test6", (void *) 77);
+    start(killer, 4000, 128, "killer", (void *)77);
 
     // // tres long
 	// start(test7, 4000, 128, "test7", (void* ) 77);
 
-	// start(test8, 4000, 128, "test8", (void *) 77);
+	start(test8, 4000, 128, "test8", (void *) 77);
 
     // // tres long
 	// start(test9, 4000, 128, "test9", (void*) 77);
@@ -63,20 +64,23 @@ void kernel_start(void)
 	// start(test13, 4000, 128, "test13", (void*) 77);
     // start(test16, 4000, 128, "test16", (void*)77);
 
-	// start(test14, 4000, 128, "test14", (void *) 77);
-	// start(test15, 4000, 128, "test15", (void *) 77);
-
+	start(test14, 4000, 128, "test14", (void *) 77);
+	start(test15, 4000, 128, "test15", (void *) 77);
 	start(test17, 4000, 128, "test17", (void*)77);
-    
+
+    /* console */
+
+    // start(test_cons_read, 4000, 128, "test_cons_read", (void *)77);
+
+    // // passe pas
 	// start(test18, 4000, 128, "test18", (void *)77);
 	// start(test19, 4000, 128, "test19", (void *)77);
 	// start(test20, 4000, 128, "test20", (void *)77);
 	// start(test21, 4000, 128, "test21", (void *)77);
 	// start(test22, 4000, 128, "test22", (void *)77);
 
-    // start(killer, 4000, 128, "killer", (void *)77);
-    // start(test_cons_read, 4000, 128, "test_cons_read", (void *)77);
-
+    /* shell */
+    // sti();
     // start(shell, 4000, 128, "shell", (void *) 77);
 
 	idle();
