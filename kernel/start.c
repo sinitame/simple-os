b34113f8262ac256c4eb75de9df391a27c294da1
Emrick Sinitambirivoutin <emrick.sinitambirivoutin@grenoble-inp.org>
2018-10-13 20:09:38 +0200

    Initial commit on personal github

--- kernel/start.c
+++ kernel/start.c
@@ -38,15 +38,15 @@ void kernel_start(void)
 	init_idle();
 
     /* processus */
-	start(test0, 4000, 128, "test0", (void *) 77);
-	start(test1, 4000, 128, "test1", (void *) 77);
-	start(test2, 4000, 128, "test2", (void *) 77);
-	start(test3, 4000, 128, "test3", (void *) 77);
-	start(test4, 4000, 128, "test4", (void *) 77);
-	start(test5, 4000, 128, "test5", (void *) 77);
-	start(test6, 4000, 128, "test6", (void *) 77);
+//	start(test0, 4000, 128, "test0", (void *) 77);
+//	start(test1, 4000, 128, "test1", (void *) 77);
+//	start(test2, 4000, 128, "test2", (void *) 77);
+//	start(test3, 4000, 128, "test3", (void *) 77);
+//	start(test4, 4000, 128, "test4", (void *) 77);
+//	start(test5, 4000, 128, "test5", (void *) 77);
+//	start(test6, 4000, 128, "test6", (void *) 77);
 
-    start(killer, 4000, 128, "killer", (void *)77);
+  //  start(killer, 4000, 128, "killer", (void *)77);
 
     // // tres long
 	// start(test7, 4000, 128, "test7", (void* ) 77);
@@ -59,15 +59,15 @@ void kernel_start(void)
 
     /* file de message */
 
-	start(test12, 4000, 128, "test12", (void *) 77);
+//	start(test12, 4000, 128, "test12", (void *) 77);
 
     // // -- ne passe pas
 	// start(test13, 4000, 128, "test13", (void*) 77);
     // start(test16, 4000, 128, "test16", (void*)77);
 
-	start(test14, 4000, 128, "test14", (void *) 77);
-	start(test15, 4000, 128, "test15", (void *) 77);
-	start(test17, 4000, 128, "test17", (void*)77);
+//	start(test14, 4000, 128, "test14", (void *) 77);
+//	start(test15, 4000, 128, "test15", (void *) 77);
+//	start(test17, 4000, 128, "test17", (void*)77);
 
     /* console */
 
@@ -81,8 +81,8 @@ void kernel_start(void)
 	// start(test22, 4000, 128, "test22", (void *)77);
 
     /* shell */
-    // sti();
-    // start(shell, 4000, 128, "shell", (void *) 77);
+     sti();
+     start(shell, 4000, 128, "shell", (void *) 77);
 
 	idle();
 
